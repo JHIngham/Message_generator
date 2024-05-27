8fe8ce4da8aedae90e1795c34ed2799da8e0f9a5
JHIngham <115948721+JHIngham@users.noreply.github.com>
2024-05-27 17:35:47 +0000

    Removed unessassary fuctions

--- MessageGen.js
+++ MessageGen.js
@@ -3,24 +3,15 @@ const noun = ['chair', 'house', 'zebra', 'plant'];
 const adj = ['good', 'big', 'small', 'scary'];
 
 
-const randomVerb = (verb) => {
-    let verbIndex = Math.floor(Math.random() * verb.length);
-    return verb[verbIndex];
+const random = (arr) => {
+    let index = Math.floor(Math.random() * arr.length);
+    return arr[index];
 };
 
-const randomNoun = (noun) => {
-    let nounIndex = Math.floor(Math.random() * noun.length);
-    return noun[nounIndex];
-};
 
-const randomAdj = (adj) => {
-    let adjIndex = Math.floor(Math.random() * adj.length);
-    return adj[adjIndex];
-};
+const joke = `Your mother looks like a ${random(adj)} ${random(noun)}!`;
 
-const joke = `Your mother looks like a ${randomAdj(adj)} ${randomNoun(noun)}!`;
-
-const inspQuote = `Dont't let the ${randomAdj(adj)} ${randomNoun(noun)} get you down. Just ${randomVerb(verb)} it.`;
+const inspQuote = `Dont't let the ${random(adj)} ${random(noun)} get you down. Just ${random(verb)} it.`;
 
 
 console.log(inspQuote);
